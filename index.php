82ccace90d599f322c50d7ac293c5abc6f5a41f4
chirizcc <chirizcc@gamil.com>
2016-11-02 19:01:33 +0800

    mvc 2.0

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
 
-// 导入类
-require './configs/config.php';
-require './controllers/IndexController.class.php';
-require './controllers/UserController.class.php';
-require './controllers/GoodsController.class.php';
+// 自动导入类
+function __autoload($classname)
+{
+	if(file_exists('./controllers/'.$classname.'.class.php')) {
+		require './controllers/'.$classname.'.class.php';
+	}elseif(file_exists('./models/'.$classname.'.class.php')) {
+		require './models/'.$classname.'.class.php';
+	}else {
+		header('HTTP/1.0 404 not fount');
+		echo '<h1>404 NOT FOUNT</h1>';
+		die;
+	}
+}
 
 // 接收参数
 $c = empty($_GET['c']) ? 'Index' : $_GET['c'];
 $a = empty($_GET['a']) ? 'index' : $_GET['a'];
 
 // 实例化
-$clasname = $c.'Controller';
-$controller = new $clasname();
+$classname = $c.'Controller';
+$controller = new $classname();
 
 $controller->$a();
